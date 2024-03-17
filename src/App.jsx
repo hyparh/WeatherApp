c538dbb87767cf05ff146def58719b2ddabb9dc7
hyparh <hyparh@gmail.com>
2024-03-17 21:12:11 +0200

    fetch my city, added pressure info

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,32 @@ function App() {
   const [weatherDescription, setWeatherDescription] = useState(null);
   const [sunriseTime, setSunriseTime] = useState(null);
   const [sunsetTime, setSunsetTime] = useState(null);
+  const [pressure, setPressure] = useState(null);
 
   useEffect(() => {
     const fetchTemperature = async () => {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=${API_KEY}`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Novi Han&appid=${API_KEY}`);
       const data = await response.json();
 
+      //temp
       const kelvinTemp = data.main.temp;
-      const celsiusTemp = kelvinTemp - 273.15; 
-      setTemperature(celsiusTemp.toFixed(2));
+      const celsiusTemp = kelvinTemp - 273.15;
+      setTemperature(celsiusTemp.toFixed(1));
 
+      //pressure
+      const pressure = data.main.pressure;
+      setPressure(pressure);
+
+      //wind
       const mps = data.wind.speed;
       const kmph = mps * 3.6;
-      setWindSpeed(kmph.toFixed(2));
+      setWindSpeed(kmph.toFixed(0));
 
+      //humidity
       const humidity = data.main.humidity;
       setHumidity(humidity);
 
+      //precipitation
       const weatherDescription = data.weather[0].description;
       setWeatherDescription(weatherDescription);
 
@@ -41,13 +50,12 @@ function App() {
       } else if (weatherId >= 600 && weatherId <= 622) {
         precipitationIndication = "Възможно е да има снеговалеж";
       }
-    
+
       setPrecipitationIndication(precipitationIndication);
 
+      //sunrise / sunset
       const sunriseTime = data.sys.sunrise;
       const sunsetTime = data.sys.sunset;
-      console.log(sunriseTime);
-      console.log(sunsetTime);
       const sunriseTimeFormatted = format(new Date(sunriseTime * 1000), 'HH:mm');
       const sunsetTimeFormatted = format(new Date(sunsetTime * 1000), 'HH:mm');
       setSunriseTime(sunriseTimeFormatted);
@@ -59,8 +67,9 @@ function App() {
 
   return (
     <div>
-      <h1>Времето в София</h1>
+      <h1>Времето в Нови хан</h1>
       {temperature && <p>Температура: {temperature}°C</p>}
+      {pressure && <p>Атмосферно налягане: {pressure} hPa</p>}
       {windSpeed && <p>Скорост на вятъра: {windSpeed} km/h</p>}
       {humidity && <p>Влажност: {humidity}%</p>}
       {weatherDescription && <p>Описание на времето: {weatherDescription}</p>}
